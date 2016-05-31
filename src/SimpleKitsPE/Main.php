d7e64dfd3925cf76cc30d15032204c708a2e59a7
paetti <paettipat@gmail.com>
2016-05-31 16:42:37 +0200

    Update Main.php

--- src/SimpleKitsPE/Main.php
+++ src/SimpleKitsPE/Main.php
@@ -44,78 +44,85 @@ class Main extends PluginBase implements Listener{
             case "diamondkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(276,0,1));
             $sender->getInventory()->addItem(Item::get(310,0,1));
             $sender->getInventory()->addItem(Item::get(311,0,1));
             $sender->getInventory()->addItem(Item::get(312,0,1));
             $sender->getInventory()->addItem(Item::get(313,0,1));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Diamond Kit! Check your Inventory for the Items!");
+               }
             return true;
             
             case "ironkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(267,0,1));
             $sender->getInventory()->addItem(Item::get(306,0,1));
             $sender->getInventory()->addItem(Item::get(307,0,1));
             $sender->getInventory()->addItem(Item::get(308,0,1));
             $sender->getInventory()->addItem(Item::get(309,0,1));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Iron Kit! Check your Inventory for the Items!");
+               }
             return true;
             
             case "goldenkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(283,0,1));
             $sender->getInventory()->addItem(Item::get(314,0,1));
             $sender->getInventory()->addItem(Item::get(315,0,1));
             $sender->getInventory()->addItem(Item::get(316,0,1));
             $sender->getInventory()->addItem(Item::get(317,0,1));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Golden Kit! Check your Inventory for the Items!");
+               }
             return true;
             
             case "chainkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(267,0,1));
             $sender->getInventory()->addItem(Item::get(302,0,1));
             $sender->getInventory()->addItem(Item::get(303,0,1));
             $sender->getInventory()->addItem(Item::get(304,0,1));
             $sender->getInventory()->addItem(Item::get(305,0,1));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Chain Kit! Check your Inventory for the Items!");
+            }
             return true;
             
             case "leatherkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(268,0,1));
             $sender->getInventory()->addItem(Item::get(298,0,1));
             $sender->getInventory()->addItem(Item::get(299,0,1));
             $sender->getInventory()->addItem(Item::get(300,0,1));
             $sender->getInventory()->addItem(Item::get(301,0,1));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Leather Kit! Check your Inventory for the Items!");
+               }
             return true;
      
             case "foodkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(260,0,10));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Food Kit! Check your Inventory for the Items!");
+               }
             return true;
             
             case "bowkit":
                if (!($sender instanceof Player)) {
                      $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Only available ingame!");
-               }
+               } else {
             $sender->getInventory()->addItem(Item::get(261,0,1));
             $sender->getInventory()->addItem(Item::get(262,0,128));
             $sender->sendMessage(TextFormat::GREEN . "[SimpleKitsPE] Got Bow Kit! Check your Inventory for the Items!");
+               }
             return true;
             
 		}
